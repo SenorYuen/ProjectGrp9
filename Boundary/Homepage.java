339528aa1fdb0459652972b7f050f359034025e9
SenorYuen <adam.yuen@ucalgary.ca>
2024-11-25 21:54:37 -0700

    registration page in progress and homepage improved

--- Boundary/Homepage.java
+++ Boundary/Homepage.java
@@ -14,15 +14,17 @@ public class Homepage extends JPanel {
     private JLabel passwordLabel;
     private JLabel guestBypassLabel;
     private JLabel developerNote;
+    private JLabel background;
     private static final long serialVersionUID = 1L;
 
     public Homepage(JFrame mainWindow) {
+        setLayout(null);
         // Label creation for the homepage
         theaterTitle = new JLabel("I AM NOT RACIST (change title later)");
         theaterTitle.setHorizontalAlignment(SwingConstants.CENTER);
         theaterTitle.setForeground(Color.BLACK);
         theaterTitle.setFont(new Font("Calibri", Font.BOLD, 30));
-        theaterTitle.setBounds(370, 50, 700, 30);
+        theaterTitle.setBounds(370, 130, 700, 30);
         add(theaterTitle);
 
         // Label creation for the username entry field
@@ -30,7 +32,7 @@ public class Homepage extends JPanel {
         loginLabel.setHorizontalAlignment(SwingConstants.CENTER);
         loginLabel.setForeground(Color.BLACK);
         loginLabel.setFont(new Font("Calibri", Font.PLAIN, 21));
-        loginLabel.setBounds(520, 125, 400, 30);
+        loginLabel.setBounds(520, 205, 400, 30);
         add(loginLabel);
 
         // Text field entry creation for username
@@ -38,7 +40,7 @@ public class Homepage extends JPanel {
         loginBox.setHorizontalAlignment(SwingConstants.CENTER);
         loginBox.setForeground(Color.BLACK);
         loginBox.setBackground(Color.GRAY);
-        loginBox.setBounds(520, 150, 400, 30);
+        loginBox.setBounds(520, 230, 400, 30);
         add(loginBox);
 
         // Label creation for the password text field
@@ -46,14 +48,14 @@ public class Homepage extends JPanel {
         passwordLabel.setHorizontalAlignment(SwingConstants.CENTER);
         passwordLabel.setForeground(Color.BLACK);
         passwordLabel.setFont(new Font("Calibri", Font.PLAIN, 21));
-        passwordLabel.setBounds(520, 200, 400, 30);
+        passwordLabel.setBounds(520, 280, 400, 30);
         add(passwordLabel);
 
         // Password field creation
         passwordBox = new JPasswordField();
         passwordBox.setForeground(Color.BLACK);
         passwordBox.setBackground(Color.GRAY);
-        passwordBox.setBounds(520, 225, 400, 30);
+        passwordBox.setBounds(520, 305, 400, 30);
         add(passwordBox);
 
         // Login/submit button creation
@@ -61,7 +63,7 @@ public class Homepage extends JPanel {
         submitLoginButton.setHorizontalAlignment(SwingConstants.CENTER);
         submitLoginButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
         submitLoginButton.setFont(new Font("Calibri", Font.PLAIN, 23));
-        submitLoginButton.setBounds(520, 260, 400, 30);
+        submitLoginButton.setBounds(520, 340, 400, 30);
         submitLoginButton.setForeground(Color.BLACK);
         add(submitLoginButton);
 
@@ -70,7 +72,7 @@ public class Homepage extends JPanel {
         signUpButton.setHorizontalAlignment(SwingConstants.CENTER);
         signUpButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
         signUpButton.setFont(new Font("Calibri", Font.PLAIN, 23));
-        signUpButton.setBounds(520, 320, 400, 30);
+        signUpButton.setBounds(520, 400, 400, 30);
         signUpButton.setForeground(Color.BLACK);
         add(signUpButton);
 
@@ -79,17 +81,22 @@ public class Homepage extends JPanel {
         guestBypassLabel.setHorizontalAlignment(SwingConstants.CENTER);
         guestBypassLabel.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
         guestBypassLabel.setFont(new Font("Calibri", Font.PLAIN, 23));
-        guestBypassLabel.setBounds(520, 380, 400, 30);
+        guestBypassLabel.setBounds(520, 460, 400, 30);
         guestBypassLabel.setForeground(Color.BLACK);
         add(guestBypassLabel);
 
-        // Developer note added
+        // Developer note creation
         developerNote = new JLabel("Developed by: Arsalan Khaleel, Fahmi Sardar, Zaid Shaikh, Adam Yuen");
         developerNote.setHorizontalAlignment(SwingConstants.CENTER);
-        developerNote.setForeground(Color.BLACK);
+        developerNote.setForeground(Color.WHITE);
         developerNote.setFont(new Font("Calibri", Font.PLAIN, 12));
-        developerNote.setBounds(520, 650, 400, 30);
+        developerNote.setBounds(520, 530, 400, 30);
         add(developerNote);
+
+        background = new JLabel("");
+        background.setBounds(100, 0, 1366, 768);
+        background.setIcon(new ImageIcon(Homepage.class.getResource("theaterVector.jpg")));
+        add(background);
 
         signUpButton.addMouseListener(new MouseAdapter() {
 			@Override
--- Boundary/Registration.java
+++ Boundary/Registration.java
@@ -5,15 +5,109 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Registration extends JPanel {
-    private JLabel test;
+    private JLabel header;
+    private JLabel emailLabel;
+    private JTextField emailBox;
+    private JLabel passwordLabel;
+    private JPasswordField passwordBox;
+    private JLabel confirmPasswordLabel;
+    private JPasswordField confirmPasswordBox;
+    private JLabel signUpButton;
+    private JLabel backButton;
+    private JLabel developerNote;
+    private static final long serialVersionUID = 1L;
 
     public Registration(JFrame mainWindow) {
-        test = new JLabel("Registration Page");
-        test.setHorizontalAlignment(SwingConstants.CENTER);
-        test.setForeground(Color.BLACK);
-        test.setFont(new Font("Calibri", Font.BOLD, 30));
-        test.setBounds(370, 50, 700, 30);
-        add(test);
+        setLayout(null);
+        // Creating a label for the page title
+        header = new JLabel("Registration Page");
+        header.setHorizontalAlignment(SwingConstants.CENTER);
+        header.setForeground(Color.BLACK);
+        header.setFont(new Font("Calibri", Font.BOLD, 30));
+        header.setBounds(520, 50, 400, 30);
+        add(header);
+
+        // Label creation for the username entry field
+        emailLabel = new JLabel("Enter your email");
+        emailLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        emailLabel.setForeground(Color.BLACK);
+        emailLabel.setFont(new Font("Calibri", Font.PLAIN, 21));
+        emailLabel.setBounds(520, 125, 400, 30);
+        add(emailLabel);
+
+        // Text field entry creation for username/email
+        emailBox = new JTextField();
+        emailBox.setHorizontalAlignment(SwingConstants.CENTER);
+        emailBox.setForeground(Color.BLACK);
+        emailBox.setBackground(Color.GRAY);
+        emailBox.setBounds(520, 150, 400, 30);
+        add(emailBox);
+
+        // Label creation for the password text field
+        passwordLabel = new JLabel("Enter your password");
+        passwordLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        passwordLabel.setForeground(Color.BLACK);
+        passwordLabel.setFont(new Font("Calibri", Font.PLAIN, 21));
+        passwordLabel.setBounds(520, 200, 400, 30);
+        add(passwordLabel);
+
+        // Password field creation
+        passwordBox = new JPasswordField();
+        passwordBox.setForeground(Color.BLACK);
+        passwordBox.setBackground(Color.GRAY);
+        passwordBox.setBounds(520, 225, 400, 30);
+        add(passwordBox);
+
+        // Label creation for the password text field
+        confirmPasswordLabel = new JLabel("Confirm your password");
+        confirmPasswordLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        confirmPasswordLabel.setForeground(Color.BLACK);
+        confirmPasswordLabel.setFont(new Font("Calibri", Font.PLAIN, 21));
+        confirmPasswordLabel.setBounds(520, 275, 400, 30);
+        add(confirmPasswordLabel);
+
+        // Password field creation
+        confirmPasswordBox = new JPasswordField();
+        confirmPasswordBox.setForeground(Color.BLACK);
+        confirmPasswordBox.setBackground(Color.GRAY);
+        confirmPasswordBox.setBounds(520, 300, 400, 30);
+        add(confirmPasswordBox);
+
+        // Sign up button creation
+        signUpButton = new JLabel("Sign Up");
+        signUpButton.setHorizontalAlignment(SwingConstants.CENTER);
+        signUpButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        signUpButton.setFont(new Font("Calibri", Font.PLAIN, 23));
+        signUpButton.setBounds(520, 330, 400, 30);
+        signUpButton.setForeground(Color.BLACK);
+        add(signUpButton);
+
+        // Sign up button creation
+        backButton = new JLabel("Return Home");
+        backButton.setHorizontalAlignment(SwingConstants.CENTER);
+        backButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        backButton.setFont(new Font("Calibri", Font.PLAIN, 23));
+        backButton.setBounds(20, 20, 200, 30);
+        backButton.setForeground(Color.BLACK);
+        add(backButton);
+
+        // Creation of a developer note
+        developerNote = new JLabel("Developed by: Arsalan Khaleel, Fahmi Sardar, Zaid Shaikh, Adam Yuen");
+        developerNote.setHorizontalAlignment(SwingConstants.CENTER);
+        developerNote.setForeground(Color.BLACK);
+        developerNote.setFont(new Font("Calibri", Font.PLAIN, 12));
+        developerNote.setBounds(520, 650, 400, 30);
+        add(developerNote);
+
+        backButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				Homepage homepage = new Homepage(mainWindow);
+				mainWindow.setContentPane(homepage);
+				mainWindow.revalidate();
+			}
+		});
+
         setVisible(true);
     }
     
