b835557da37516b151664c6e0993a6c681f11d4e
SenorYuen <adam.yuen@ucalgary.ca>
2024-11-25 17:05:11 -0700

    added GUI prototype

---
+++ Implementation/GUIControl.java
@@ -0,0 +1,20 @@
+package Implementation;
+
+import javax.swing.JFrame;
+
+public class GUIControl extends JFrame {
+    private static Homepage homepage;
+    private static final long serialVersionUID = 1L;
+
+    GUIControl() {
+        homepage = new Homepage();
+        setTitle("title");
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(1366, 768);
+        setContentPane(homepage);
+        setLocationRelativeTo(null);
+        setLayout(null);
+        setVisible(true);
+    }
+}
---
+++ Implementation/TempMain.java
@@ -0,0 +1,8 @@
+package Implementation;
+
+public class TempMain {
+    public static void main(String[] args) {
+        TempRunningThing e = new TempRunningThing();
+    }
+}
+
