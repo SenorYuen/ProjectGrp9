5011c803fbfc2062eb4c4bf2ce660d917e65acb5
SenorYuen <adam.yuen@ucalgary.ca>
2024-12-01 21:25:10 -0700

    removed all the database stuff since we will add it later in the submission

--- database-notworking/Implementation/Person.java
+++
@@ -1,8 +0,0 @@
-package Implementation;
-
-import java.util.ArrayList;
-
-interface Person {
-    Receipt makePayment(String theaterLocation, String movieChosen, int seatNumber, double amountPaid);
-    ArrayList<Movie> browseCatalog(Theater theater);
-}
